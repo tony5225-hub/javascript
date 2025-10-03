983a6fe6aaabf40e165c23ab7d7d80d027a229e7
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2025-10-03 06:42:08 -0300

    atualizar
    Atualizar exercícios 08 e 09

---
+++ ExerciciosJS/aula11/ex008b.js
@@ -0,0 +1,9 @@
+var vel = 90
+
+console.log (`A velocidade do veiculo é ${vel} km/h`)
+if (vel>60){
+    console.log (`Voce ultrapassou a velocidade permitida Será Multado!!`)
+}
+{
+    console.log (`Dirija sempre com cinto Segurança`)
+}
---
+++ ExerciciosJS/aula11/ex009b.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Se Viver no(a) ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Voce é brasileiro')
+}
+else {
+        console.log('Voce não é brasileiro')
+}
