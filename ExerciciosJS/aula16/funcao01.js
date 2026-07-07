ed9e25722c7198ab5addff9301c9f685349a5c9c
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2026-07-07 06:28:47 -0300

    Update funcao01.js
    atualizar

--- ExerciciosJS/aula16/funcao01.js
+++ ExerciciosJS/aula16/funcao01.js
@@ -10,4 +10,6 @@ let res = parimpar(6)
 console.log (res)
 pode escrever sem precisar digitar a varivel (LET) veja abaixo
 */
-console.log(parimpar(3)) // mostrando que log impar é  a chamada do JS e 3 é o parametro. analisando a function: par impar é a chamada e numero 3 é o parametro que na formula: function parimpar(n) dentro n o 3 vai para if ser analisado se é para ou impar que é retorno  e console.log(parimpar(3)) me da resultado quando aplico f8
+
+
+console.log(parimpar(3)) // mostrando que log impar é a chamada do JS e 3 é o parametro. analisando a function: par impar é a chamada e numero 3 é o parametro que na formula: function parimpar(n) dentro n o 3 vai para if ser analisado se é par ou impar que é retorno  e console.log(parimpar(3)) me da resultado quando aplico f8
