a9e891d6675193f107cee7e0a250fc9f2bd302f7
Antonio Fernando <70606080+tony5225-hub@users.noreply.github.com>
2026-07-11 06:43:17 -0300

    atualizar
    atualizar exercicios

--- ExerciciosJS/aula16/funcao03.js
+++ ExerciciosJS/aula16/funcao03.js
@@ -1,4 +1,4 @@
-let v = function (x){// aqui exemplificamos uma função dentro de uma variável e na variável foi passado um parametro que é x e executou dentro da função
+let v = function (x){ // aqui exemplificamos uma função dentro de uma variável e na variável foi passado um parametro que é x e executou dentro da função
 
     return x*2
 }
